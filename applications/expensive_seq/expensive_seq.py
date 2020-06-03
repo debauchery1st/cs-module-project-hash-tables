5ba9026af8392fa8f84f563afc67be48cfa1dea4
Trevor Martin <trevor.j.martin@gmail.com>
2020-06-03 11:44:11 -0400

    expensive sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,21 @@
-# Your code here
+import math
+
+# https://mail.python.org/pipermail/python-list/2000-March/048085.html
+expensive_answers = dict()
 
 
 def expensive_seq(x, y, z):
-    # Your code here
-
+    key = (x, y, z)
+    ans = expensive_answers.get(key, None)
+    if ans is None:
+        if x <= 0:
+            ans = y + z
+        else:
+            ans = expensive_seq(x-1, y+1, z) + \
+                expensive_seq(x-2, y+2, z*2) + \
+                expensive_seq(x-3, y+3, z*3)
+        expensive_answers[key] = ans
+    return ans
 
 
 if __name__ == "__main__":
